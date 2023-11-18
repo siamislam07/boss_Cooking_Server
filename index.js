658f4e04e570a91b37635d1b077ccf6f44cc548a
siamislam07 <siamislam1970@gmail.com>
2023-11-18 12:21:58 +0600

    added verfiyadmin middleware

--- index.js
+++ index.js
@@ -43,24 +43,52 @@ async function run() {
         const verifyToken = (req, res, next) => {
             console.log('inside verify token', req.headers.authorization);
             if(!req.headers.authorization) {
-                return res.status(401).send({ message: 'forbidden' })
+                return res.status(401).send({ message: 'unauthorized access' })
             }
             const token = req.headers.authorization.split(' ')[1]
             jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
                 if (err) {
-                    return res.status(401).send({message:'forbidden access'})
+                    return res.status(401).send({message:'unauthorized access'})
                 }
                 req.decoded = decoded;
                 next()
             })
         }
 
+        const verifyAdmin = async(req, res, next)=>{
+            const email = req.decoded.email
+            const query = {emial: email}
+            const user = await userCollection.findOne(query)
+            const isAdmin = user?.role === 'admin'
+            if (!isAdmin) {
+                return res.status(403).send({message: 'forbidden access'})
+            }
+            next()
+        }
+
         //users related api
-        app.get('/users',verifyToken, async (req, res) => {
+        app.get('/users',verifyToken,verifyAdmin, async (req, res) => {
             console.log(req.headers);
             const result = await userCollection.find().toArray()
             res.send(result)
         })
+
+        app.get('/users/admin/:email',verifyToken, async(req,res)=>{
+            const email = req.params.email
+
+            if (email !== req.decoded.email) {
+                return res.status(403).send({message: 'forbidden access'})
+            }
+
+            const query = {email: email}
+            const user = await userCollection.findOne(query)
+            let admin = false
+            if (user) {
+                admin = user?.role === 'admin'
+            }
+            res.send({admin})
+        })
+
         app.post('/users', async (req, res) => {
             const user = req.body
             // insert email if user does not exists
@@ -76,7 +104,7 @@ async function run() {
         })
         //user related api ends
 
-        app.patch('/users/admin/:id', async (req, res) => {
+        app.patch('/users/admin/:id',verifyToken,verifyAdmin, async (req, res) => {
             const id = req.params.id
             const filter = { _id: new ObjectId(id) }
             const updatedDoc = {
@@ -88,7 +116,7 @@ async function run() {
             res.send(result)
         })
 
-        app.delete('/users/:id', async (req, res) => {
+        app.delete('/users/:id',verifyToken, verifyAdmin, async (req, res) => {
             const id = req.params.id
             const query = { _id: new ObjectId(id) }
             const result = await userCollection.deleteOne(query)
